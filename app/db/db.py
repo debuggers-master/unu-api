c674efe42254a1362e1a8f6feef5066706f89dbf
emanuelosva <emanuelosva@gmail.com>
2020-08-17 23:34:35 -0500

    Add CRUD class

--- app/db/db.py
+++ app/db/db.py
@@ -60,3 +60,47 @@ def jsonify(data: BSON) -> dict:
     """
 
     return json.loads(dumps(data))
+
+
+# ------------------------ CRUD class ------------------------ #
+class CRUD:
+    """
+    Crud operations.
+
+    Params:
+    ------
+    collection: mongo_collection
+        The mongo collection for CRUD
+    """
+
+    def __init__(self, collection):
+        self.coll = collection
+
+    async def create(self, document_data: dict) -> str:
+        """
+        Create a new document in collection.
+        """
+        created = self.coll.insert_one(document_data)
+        return str(created.inserted_id)
+
+    async def update(self, query: dict, document_data: dict) -> str:
+        """
+        Update an existing document.
+        """
+        updated = self.coll.update_one(query, {"$set": document_data})
+        return str(updated.modified_count)
+
+    async def add_to_set(self, query: dict, array_name: str, data: any) -> str:
+        """
+        Add a new item to a list within a document.
+        """
+        operation = {"$addToSet": {f"{array_name}": data}}
+        updated = self.coll.update_one(query, operation)
+        return str(updated.modified_count)
+
+    async def delete(self, query: dict) -> None:
+        """
+        Delete a existing document
+        """
+
+        await self.coll.delete_one(query)

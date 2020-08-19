6c4ef93bec05158a33ced72f3f0f88bbc6f0ec1a
Mario Barbosa <marioabarbosam@gmail.com>
2020-08-19 02:06:46 -0500

    fix typo endpoint

--- app/api/v1/routes/events.py
+++ app/api/v1/routes/events.py
@@ -13,13 +13,12 @@ from db.events import update_event
 router = APIRouter()
 
 
-@router.post("information", status_code=204)
-async def create_information(info: InformationIn):
+@router.post("/information/", status_code=204)
+async def add_information(info: InformationIn):
     """
     Create a dict into events called information
     with **Information** Model
     """
-
     modified = await update_event(event_id=info.event_id, event_data=InformationDB(**info.dict()))
     if int(modified) < 1:
         raise HTTPException(status_code=500, detail="Internal Server Error")

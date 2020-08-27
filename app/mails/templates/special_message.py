42b40cac8cbc2f9f1de08460ea495041742baa02
emanuelosva <emanuelosva@gmail.com>
2020-08-27 12:46:18 -0500

    Add correct docstrings

--- app/mails/templates/special_message.py
+++ app/mails/templates/special_message.py
@@ -1,12 +1,22 @@
 """
-Welcome HTML template.
+Special message HTML template.
 """
 
 
 def special_message_template(
         event_name: str, message: str, event_url: str) -> str:
     """
-    Return a welcome template.
+    Return a special message template.
+
+    Params:
+    ------
+    event_name: str - The current event name.
+    message: str - The special message to registered participants.
+    event_url: str - The current event url.
+
+    Return:
+    ------
+    template: str - HTML in text format template for special message.
     """
     return """
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="width:100%;font-family:roboto, 'helvetica neue', helvetica, arial, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0"><head><meta charset="UTF-8"><meta content="width=device-width, initial-scale=1" name="viewport"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta content="telephone=no" name="format-detection"><title>Nueva plantilla</title> <!--[if (mso 16)]><style type="text/css">     a {text-decoration: none;}     </style><![endif]--> <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--> <!--[if gte mso 9]><xml> <o:OfficeDocumentSettings> <o:AllowPNG></o:AllowPNG> <o:PixelsPerInch>
--- app/mails/templates/welcome.py
+++ app/mails/templates/welcome.py
@@ -6,6 +6,14 @@ Welcome HTML template.
 def welcome_template(name: str) -> str:
     """
     Return a welcome template.
+
+    Params:
+    ------
+    name: str - The name of the new user.
+
+    Return:
+    ------
+    template: str - HTML text format template for welcome email.
     """
     return """
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="width:100%;font-family:lato, 'helvetica neue', helvetica, arial, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0"><head><meta charset="UTF-8"><meta content="width=device-width, initial-scale=1" name="viewport"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta content="telephone=no" name="format-detection"><title>Nueva plantilla de correo electrónico 2020-08-25</title> <!--[if (mso 16)]><style type="text/css">     a {text-decoration: none;}     </style><![endif]--> <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--> <!--[if gte mso 9]><xml> <o:OfficeDocumentSettings> <o:AllowPNG></o:AllowPNG>

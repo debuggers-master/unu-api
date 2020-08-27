8448b00e050439c29561ac5233b8070d5a9744cc
emanuelosva <emanuelosva@gmail.com>
2020-08-27 13:49:15 -0500

    Remove dependence of service and fix styles

--- app/auth/services.py
+++ app/auth/services.py
@@ -5,17 +5,22 @@ Authentication logic.
 from datetime import datetime, timedelta
 from typing import Optional
 from uuid import uuid4
+
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel, Field  # pylint: disable-msg=E0611
 from jose import JWTError, jwt
 from passlib.context import CryptContext
-from pydantic import BaseModel, Field  # pylint: disable-msg=E0611
 
 from config import settings  # pylint: disable-msg=E0611
-from api.v1.services.users import UserService
 from schemas.users import UserOut, UserIn
+from db.db import get_collection, CRUD
 
-# Auth constants
+
+###########################################
+##             Auth Settings             ##
+###########################################
+
 SECRET = settings.SECRET_JWT
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 45
@@ -26,15 +31,25 @@ credentials_exception = HTTPException(
     headers={"WWW-Authenticate": "Bearer"},
 )
 
-# Auth instances
+
+###########################################
+##            Security Instances         ##
+###########################################
+
 pwd_context = CryptContext(schemes=["bcrypt"])
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-# User service to DB fuctions
-UserMethos = UserService()
+
+###########################################
+##          Users CRUD Instance          ##
+###########################################
+
+users_crud = CRUD(get_collection("users"))
 
 
-# ---------------- Token Schemas ------------------- #
+###########################################
+##              Token Schemas            ##
+###########################################
 
 class Token(BaseModel):
     """
@@ -51,7 +66,9 @@ class TokenData(BaseModel):
     email: Optional[str] = None
 
 
-# ---------------- Auth helpers ------------------- #
+###########################################
+##          Helper Auth Functions        ##
+###########################################
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """
@@ -59,10 +76,8 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
     Params:
     ------
-    plain_password: str
-        The plain password from the request form.
-    hashed_password: str
-        The hashed password stored in db.
+    plain_password: str - The plain password from the request form.
+    hashed_password: str -The hashed password stored in db.
 
     Return:
     ------
@@ -74,17 +89,15 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def hash_password(plain_password: str) -> str:
     """
-    Verify the enter password with the hashed password stored in db.
+    Generate a hash of the current password.
 
     Params:
     ------
-    plain_password: str
-        The plain password from the request form.
+    plain_password: str - The plain password from the request form.
 
     Return:
     ------
-    hashed_password: str
-        The password hashed.
+    hashed_password: str - The password hashed.
     """
 
     return pwd_context.hash(plain_password)
@@ -92,22 +105,19 @@ def hash_password(plain_password: str) -> str:
 
 async def authenticate_user(email: str, password: str) -> UserOut:
     """
-    Auhenticate the user sended in the form request.
+    Auhenticate the user recived in the request.
 
     Params:
     ------
-    email: str
-        The user email
-    password: str
-        The password email
+    email: str - The user email
+    password: str - The password email
 
     Return:
     ------
-    usut
-        The user class. If not authenticated, returns False.
+    user: UserOut - The user class. If not authenticated, returns False.
     """
 
-    user = await UserMethos.get_user(email=email)
+    user = await users_crud.find({"email": email})
     if not user:
         return False
     if not verify_password(password, user.get("password")):
@@ -129,34 +139,36 @@ def create_access_token(
 
     Return:
     ------
-    encode_jwt: str
-        The encoded json web token.
+    encode_jwt: bytes - The encoded json web token.
     """
 
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + expires_delta
+
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET, algorithm=ALGORITHM)
     return encoded_jwt
 
 
+###########################################
+##        Authentication Middleware      ##
+###########################################
+
 async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserOut:
     """
-    Verify the user in token and return user if token is valid.
+    Verify the user in token and return the user if token is valid.
 
     Params:
     ------
-    taken: str
-        The jwt in the header request.
+    taken: str - The jwt in the header request.
 
     Return:
-    user: UserIn
-        The user data.
+    user: UserOut - The user data.
     """
-
     try:
         payload: dict = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
         email: str = payload.get("sub")
@@ -166,11 +178,15 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserOut:
     except JWTError:
         raise credentials_exception
 
-    user = await UserMethos.get_user(email=token_data.email)
+    user = await users_crud.find({"email": token_data.email})
     if user is None:
         raise credentials_exception
     return UserOut(**user)
 
+
+###########################################
+##             Register Function         ##
+###########################################
 
 async def register_user(user: UserIn) -> str:
     """
@@ -178,12 +194,11 @@ async def register_user(user: UserIn) -> str:
 
     Params:
     ------
-    user: UserIn
-        The principal user data.
+    user: UserIn - The principal user data.
 
     Return:
-    user: UserOut
-        The complete user data.
+    ------
+    user: UserOut - The complete user data.
     """
 
     new_user = user.dict()
@@ -193,7 +208,7 @@ async def register_user(user: UserIn) -> str:
     new_user.update({"collaborations": []})
     new_user.update({"userId": str(uuid4())})
 
-    inserted_id = await UserMethos.create_user(new_user)
+    inserted_id = await users_crud.create(new_user)
     if inserted_id is not None:
         return UserOut(**new_user)
     return False

01f64ee2f38231f0ce07136334625f95f551ca80
Mario Barbosa <marioabarbosam@gmail.com>
2020-08-29 15:13:04 -0500

    fix bugs and added documentation

--- app/main.py
+++ app/main.py
@@ -9,9 +9,40 @@ from api.v1 import api_router
 from auth.routes import auth_router
 from config import settings  # pylint: disable-msg=E0611
 
+tags_metadata = [
+    {
+        "name": "Auth",
+        "description": "Authentication module, include login and sign up endpoints",
+    },
+    {
+        "name": "Users",
+        "description": "User Module, allowed methods are get, put and delete users",
+    },
+    {
+        "name": "Organizations",
+        "description": "Organizations Module, allowed methods are get, put, post and delete organizations information",
+    },
+    {
+        "name": "Events",
+        "description": "Events Module, allowed methods are get, put, post and delete events information",
+    },
+    {
+        "name": "Participants",
+        "description": "Participants Module, allowed methods are get, put, post and delete events Participants",
+    },
+    {
+        "name": "Mails",
+        "description": "Mails Module, allowe method is post events information",
+    }
+]
+
+
 app = FastAPI(
     title=settings.APP_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    description="An awesome app to create and manage your events ",
+    version="1.0",
+    openapi_tags=tags_metadata
 )
 
 ###########################################
